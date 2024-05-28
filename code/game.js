52d1bcbe137c0d3da7a47baf3979d56b65e8bd80
Luka <128266839+UnPolloLoco@users.noreply.github.com>
2024-05-28 10:22:52 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -2,7 +2,7 @@ scene('game', () => {
   const apple = add([
     sprite('apple'),
     pos(SCALE*3, SCALE*3),
-    scale((SCALE/64) * 3), // SCALE divided by sprite width, multiplied by the size change
+    scale((SCALE/500) * 3), // SCALE divided by sprite width, multiplied by the size change
     rotate(0),
     anchor('center'),
 
